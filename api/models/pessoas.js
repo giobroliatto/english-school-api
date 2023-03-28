8123bf8ef23b7815bdadaa9726fdbdb7c0cc8e60
giobroliatto <giovanibroliatto@gmail.com>
2023-03-28 00:06:19 -0300

    feat: :construction: add data validations on person's model

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,9 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Pessoas = sequelize.define('Pessoas', {
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado) {
+          if (dado.length < 3) throw new Error("o campo 'nome' deve ter mais de 3 caracteres")
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "dado do tipo 'email' inválido"
+        }
+      }
+    },
     role: DataTypes.STRING
   }, { 
     paranoid: true,
